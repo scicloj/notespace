56e91dd30d715a1a97975d0f5f6fdf1dd19a3d39
daslu <daniel.slutsky@gmail.com>
2019-12-24 15:02:27 +0200

    version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject scicloj/notespace "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject scicloj/notespace "1.0.0"
+  :description "Notebook experience in your Clojure namespace."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
