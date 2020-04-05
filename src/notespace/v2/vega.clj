1441417dee7d295efe0534cd20bd3b82f059041c
Carsten Behring <carsten.behring@gmail.com>
2020-04-05 18:42:57 +0200

    added a test

--- src/notespace/v2/vega.clj
+++ src/notespace/v2/vega.clj
@@ -9,15 +9,6 @@
   )
 
 
-(defn temp-json-file
-  "helper function convert clj vega spec to json and store it as tmp file"
-  [content]
-  (let [tmp-file (java.io.File/createTempFile "vega." ".json")]
-    (.deleteOnExit tmp-file)
-    (with-open [file (clojure.java.io/writer tmp-file)]
-      (json/write content file))
-    (.getAbsolutePath tmp-file)))
-
 
 (defn vega->hiccup [spec mode]
   "Converts a vega lite specification file into hiccup, which can be rendered via note-hiccup
