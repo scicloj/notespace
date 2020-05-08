0a6a0a40dc85491b108d763578fd4bd906e202d7
daslu <daniel.slutsky@gmail.com>
2020-05-09 01:31:59 +0300

    making sure the loading gif is available when rendering to file

--- src/notespace/v2/note.clj
+++ src/notespace/v2/note.clj
@@ -239,9 +239,14 @@
 (defn ns->out-filename [namespace]
   (format "%s/index.html" (ns->out-dir namespace)))
 
+(defn copy-waiting-gif! []
+  (copy-to-ns-target-path (clojure.java.io/resource "images/Ball-1s-24px.gif")
+                          "waiting.gif"))
+
 (defn render-to-file! [render-fn path]
   (let [path-to-use (or path (str (File/createTempFile "rendered" ".html")))
         html (page/html5 (render-fn))]
+    (copy-waiting-gif!)
     (spit path-to-use html)
     (log/info [::wrote path-to-use])
     html))
