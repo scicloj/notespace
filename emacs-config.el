d55740ea5e8638ad43cef71962fb5cbe26cee552
daslu <daniel.slutsky@gmail.com>
2020-08-24 21:21:43 +0300

    emacs config

---
+++ emacs-config.el
@@ -0,0 +1,25 @@
+(defun notespace/init-with-browser ()
+  (interactive)
+  (save-buffer)
+  (cider-interactive-notify-and-eval
+   (concat "(notespace.api/init-with-browser)")))
+
+(defun notespace/init ()
+  (interactive)
+  (save-buffer)
+  (cider-interactive-notify-and-eval
+   (concat "(notespace.api/init)")))
+
+(defun notespace/eval-and-realize-note-at-this-line ()
+  (interactive)
+  (save-buffer)
+  (cider-interactive-notify-and-eval
+   (concat "(notespace.api/eval-and-realize-note-at-line! "
+           (number-to-string (line-number-at-pos))
+           ")")))
+
+(defun notespace/eval-this-notespace ()
+  (interactive)
+  (save-buffer)
+  (cider-interactive-notify-and-eval
+   "(notespace.api/eval-this-notespace!)"))
