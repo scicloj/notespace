83e7058b647e03e67de60aa87afeee5e2b96255d
daslu <daniel.slutsky@gmail.com>
2020-09-14 13:48:04 +0300

    handling updates more carefully in renderer

--- src/notespace/renderers/gorilla_notes.clj
+++ src/notespace/renderers/gorilla_notes.clj
@@ -29,29 +29,36 @@
 
 (defn renderer [old-ctx new-ctx]
   (when-let [namespace (fx/sub-val new-ctx :last-ns-handled)]
-    (let [[old-notes new-notes]             (->> [old-ctx new-ctx]
-                                                 (map (fn [ctx]
-                                                        (fx/sub-val
-                                                         ctx
-                                                         get-in [:ns->notes namespace]))))
-          new-things                        (->> (map vector
-                                                      (range)
-                                                      (concat old-notes (repeat nil))
-                                                      new-notes)
-                                                 (filter
-                                                  (fn [[_ old-note new-note]]
-                                                    (not (= old-note new-note)))))
-          [old-n new-n]                     (->> [old-notes new-notes]
-                                                 (map count))]
-      (->> new-things
-           (run!
-            (fn [[idx _ new-note]]
-              (gn/assoc-note!
-               idx
-               (fx/sub-ctx new-ctx rendering namespace idx)
-               :broadcast? false))))
-      (when (> old-n new-n)
-        (gn/drop-tail! (- old-n new-n)
-                       :broadcast? false))
-      (Thread/sleep 100)
-      (gn/broadcast-content-ids!))))
+    (if (not= (fx/sub-val old-ctx :inputs)
+              (fx/sub-val new-ctx :inputs))
+      (do
+        (dotimes [idx (-> new-ctx
+                          (fx/sub-val get-in [:ns->notes namespace])
+                          count)]
+          (actions/rerender-note! namespace idx)))
+      (let [[old-notes new-notes] (->> [old-ctx new-ctx]
+                                       (map (fn [ctx]
+                                              (fx/sub-val
+                                               ctx
+                                               get-in [:ns->notes namespace]))))
+            new-things            (->> (map vector
+                                            (range)
+                                            (concat old-notes (repeat nil))
+                                            new-notes)
+                                       (filter
+                                        (fn [[_ old-note new-note]]
+                                          (not (= old-note new-note)))))
+            [old-n new-n]         (->> [old-notes new-notes]
+                                       (map count))]
+        (->> new-things
+             (run!
+              (fn [[idx _ new-note]]
+                (gn/assoc-note!
+                 idx
+                 (fx/sub-ctx new-ctx rendering namespace idx)
+                 :broadcast? false))))
+        (when (> old-n new-n)
+          (gn/drop-tail! (- old-n new-n)
+                         :broadcast? false))
+        (Thread/sleep 100)
+        (gn/broadcast-content-ids!)))))
