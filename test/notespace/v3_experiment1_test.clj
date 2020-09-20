882fda0ca55227938f3a513fd35fcdf91d25055e
daslu <daniel.slutsky@gmail.com>
2020-09-21 02:21:21 +0300

    updated tutorial

--- test/notespace/v3_experiment1_test.clj
+++ test/notespace/v3_experiment1_test.clj
@@ -17,10 +17,11 @@
 
 ["# Notespace v3 intro
 
-This is an experimantal incomplete draft. It should serve as a basis for discussion on our way to v3 of notespace."]
+[Notespace](https://github.com/scicloj/notespace) is a library that turns any Clojure namespace into a 'notespace' -- a namespace with a [notebook](https://en.wikipedia.org/wiki/Notebook_interface) experience. A notespace evolves in an interactive process of exploration, involving a dialogue of your editor, your REPL, your browser and your mind.
 
-(require '[notespace.api :as api :refer [A D F]]
-         '[notespace.kinds :as k])
+So, `Notespace` with a capital `N` is the name of the tool, and `notespace` with a small `n` is the notion that we just presented.
+
+Here we describe an experimental incomplete draft of Notespace Version 3."]
 
 ["## Notes
 
@@ -30,13 +31,144 @@ Here is a note:"]
    3 4
    5 6)
 
-["We see its code ant its return value rendered."]
+["Notespace renders it so that we see both the code and the evaluation result.
+
+All your namespace is seen as a sequence of toplevel forms, which are considered as notes, and rendered accordingly."]
+
+["## Basic workflow
+
+Typically, you may like to edit one or more namespaces, that may be about function definitions, tests, data exploration, etc., and have Notespace update its notebook view while these namespaces evolve. At some point, you may like to render the view as a standalone html file for documentation.
+
+See [this screencast](https://www.youtube.com/watch?v=_GcTnkhn9g0) for a recorded session. Note, however, that the note kinds defined in the screencast are a bit different from those of the current version.
+
+Let us see how to do this with the Notespace API."
+
+"### The main API functions"
+
+"#### init
+
+Use `notespace.api/init` to initialize the Notespace system's state.
+
+In the beginning of your work, you need to use it.
+
+You can also use it afterwards. That is useful if you run into an unstable state (yes, this may happen sometimes -- there may be some concurrency bugs)."
+
+"#### init-with-browser
+
+Use `notespace.api/init-with-browser` to initialize the Notespace system's state and open a browser tab with a notebook view of the last namespace touched.
+
+In the beginning of your work, you can use it instead of `init`.
+
+You can always refersh the browser tab. That is useful if it runs out of sync with the system's state (yes, this may happen sometimes -- there may be some concurrency bugs)."
+
+"#### eval-this-notespace!
+
+Use `notespace.api/eval-this-notespace!` to evaluate the current namespace, one note after another, and inform Notespace about the evaluation results, one after another.
+
+If you have a browser view open, it should show the updated rendering of the state of the notespace last touched."
+
+"#### eval-note-at-line!
+
+Use `notespace.api/eval-note-at-line!` to evaluate the note at a certain line and inform Noteapace about the evaluation result.
+
+For example, `(notespace.api/eval-note-at-line! 14)` applies that to the note at line 14.
+
+If you have a browser view open, it should show the updated rendering of the state of the notespace last touched.
+
+This is useful for re-evaluating parts of the notebook, whose evaluation result may have changed for some reason."
+
+"#### render-static-html!
+
+Use `notespace.api/render-static-html!` to render the Notespace state of the notespace last touched as a standalone html file. This html should look exactly as the dynamic browser view at the moment of rendering."]
+
+["### Editor integration
+
+For a convenient interactive experience, it is ecommended to bind the Notespace API functions to functions of your editor.
+
+You can see how to do this in Emacs in [this Elisp code](https://github.com/scicloj/notespace/blob/v3/emacs-config.el). In the future, we will probably provide similar solutions for other editors too."]
+
+["## References
+
+Clojure references such as delays, futures, promises and atoms are handled by Notespace in a special way. Below we will see how Notespace offers ways to bring the dynamic REPL experience of such constructs to the dynamic browser view.
+
+### The rule of rendering
+
+Assume that a note's evaluation result `x` is a reference. If `x` is already realized, then Notespace renders `@x` (dereferenced `x`) instead of `x`. Otherwise, you should see a mark that says it is still pending.
+
+For example:"]
+
+(def p1 (promise))
+(def p2 (promise))
+(deliver p2 14)
+p1
+p2
+
+["### More API functions
+
+Notespace offers some more API functions that allow for a dynamic experience with Clojure references."]
+
+["#### eval-and-realize-note-at-line!
+
+Use `notespace.api/eval-and-realize-note-at-line!` to evaluate the note at a certain line, realize the value if it is an unrealized derefable value, and inform Noteapace about the result.
+
+For example, `(notespace.api/eval-and-realize-note-at-line! 14)` applies that to the note at line 14."]
+
+["### Futures"]
+
+(require '[notespace.api :refer [F]])
+
+["The `F` macro allows to define a Clojure future that informs Notespace when it is realized. This allows Notespace to update its state with the dereferenced value.
+
+For example:"]
+
+(def x
+  (future
+   (Thread/sleep 2000)
+   (->> #(- (rand) 0.5)
+        (repeatedly 999)
+        (reductions +))))
+
+["The following note should render after two seconds (of waiting for `x` to be realized)."]
+
+(F (take 9 @x))
+
+["### Delays"]
+
+(require '[notespace.api :refer [D]])
+
+["The `D` macro allows to define a Clojure delay that informs Notespace when it is realized. This allows Notespace to update its state with the dereferenced value.
+
+For example:"]
+
+(D (+ 1 2))
+
+["If you called `notespace.api/eval-and-realize-note-at-line!` with the line holding this note, then you should see the number 3 there. Otherwise, you should see a mark that says it is still pending."]
+
+["### Atoms"]
+
+(require '[notespace.api :refer [A]])
+
+["The `A` function allows to define a Clojure atom that informs Notespace when it changes. This allows Notespace to update its state with the dereferenced value.
+
+For example:"]
+
+(def a
+  (atom {:x 3}))
+
+(A a)
+
+["If when rendering this notespace the user evaluated the code in the comment below, then you should see `{:x 4}`. Otherwise, you should still see `{:x 3}`."]
+
+(comment
+  (swap! a update :x inc))
 
 ["## Note kinds
 
 Each note has a kind, that determines its rendering behaviour.
 
 One way to specify a note's kind is by metadata. For example, here is a note of kind `:hiccup`."]
+
+(require '[notespace.kinds :as k])
 
 ^k/hiccup
 [:big [:big 3]]
@@ -105,15 +237,11 @@ A `:hiccup` note renders its value as Hiccup."]
 Notes of kind `:hiccup-nocode` are rendered as Hiccup too, but without showing the code. Here is an example:"]
 
 ^k/hiccup-nocode
-[:p/sparklinespot
- {:data      (->> #(- (rand) 0.5)
-                  (repeatedly 9)
-                  (reductions +))
-  :svgHeight 20}]
+[:big [:big 3]]
 
 ["### extended hiccup
 
-In fact, a `:hiccup` actually renders by an extended version of Hiccup. It supports where [gorilla-ui](https://github.com/pink-gorilla/gorilla-ui) tags.
+In fact, `:hiccup` and `:hiccup-nocode` render by an extended version of Hiccup, that supports [gorilla-ui](https://github.com/pink-gorilla/gorilla-ui) tags.
 
 Here are some examples."]
 
@@ -121,7 +249,10 @@ Here are some examples."]
 [:div
  [:h5 "Plot:"]
  [:p/sparklinespot
-  {:data [5, 10, 5, 20, 10] :limit 5 :svgWidth 100 :svgHeight 20 :margin 5}]]
+  {:data      (->> #(- (rand) 0.5)
+                   (repeatedly 99)
+                   (reductions +))
+   :svgHeight 20}]]
 
 ^k/hiccup
 (into [:ul]
@@ -149,48 +280,17 @@ Here are some examples."]
 
 ["## Other ways to specify note kinds.
 
-Another way is to assign a kind to a note is to include it in a vector beginning with a keyword. This is compatible with the approach of [Oz](https://github.com/metasoarous/oz)."]
+A note's kinds can also be specified by including it in a vector beginning with a keyword. This is inspired by the approach of [Oz](https://github.com/metasoarous/oz), and should allow some partial compatibility with Oz notebooks."]
 
 [:hiccup [:big [:big 3]]]
-
-["Most notes are assigned the default kind `:naive` automatically."]
-
-(+ 1 2)
 
 ["Notes which are sequential forms beginning with a string (e.g., `[\"Hello!\" \"How are you?\"]`) are assigned the default kind `:md-nocode`  automatically."
  "This very text you are reading is an example of a note of that kind."]
 
-["## References
+["All other notes witn no explicit metadata are assigned the kind `:naive` automatically."]
 
-Pending refs, such as delays, futures and promises, are rendered in a special way, taking into account whether they have been realized or not."]
+(+ 1 2)
 
-^k/void
-(def x
-  (F
-   (Thread/sleep 2000)
-   (->> #(- (rand) 0.5)
-        (repeatedly 999)
-        (reductions +))))
+["## Interactive input and reactive notes"]
 
-(F (take 9 @x))
-
-^k/hiccup
-(F
- (let [y @x]
-   (Thread/sleep 2000)
-   [:p/sparklinespot
-    {:data      y
-     :svgHeight 20}]))
-
-^k/hiccup
-(D
- (Thread/sleep 1000)
- [:h3 (+ 1 2)])
-
-(def a
-  (atom {:x 3}))
-
-(A a)
-
-(comment
-  (swap! a update :x inc))
+["Coming soon."]
