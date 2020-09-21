22bd615cf130f519cc574ca54ab4034c90eae67c
daslu <daniel.slutsky@gmail.com>
2020-09-21 21:55:00 +0300

    removed unncessary dep

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [scicloj/tablecloth "4.04"]
                  [differ "0.3.3"]
                  [hiccup "1.0.5"]
                  [zprint "0.5.2"]
