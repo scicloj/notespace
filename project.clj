05aa2459a8b218e3fafb061351895921b6ed5b46
daslu <daniel.slutsky@gmail.com>
2020-09-21 14:25:25 +0300

    update gorilla-notes dep

--- project.clj
+++ project.clj
@@ -25,4 +25,4 @@
                  [org.clojure/core.async "1.1.587"]
                  [cljfx "1.7.5"]
                  [org.clojure/core.cache "1.0.207"]
-                 [scicloj/gorilla-notes "0.4.0-SNAPSHOT"]])
+                 [scicloj/gorilla-notes "0.4.0"]])
