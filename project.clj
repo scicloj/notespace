54950f5c89a26c382a0f369142cfad521c45a1ff
daslu <daniel.slutsky@gmail.com>
2021-02-17 15:08:36 +0200

    updated gorilla-notes version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [metosin/jsonista "0.2.5" ]
+                 [metosin/jsonista "0.2.5"]
                  [org.clojure/tools.reader "1.3.2"]
                  [org.clojure/core.async "1.1.587"]
                  [cljfx "1.7.5"]
@@ -12,7 +12,7 @@
                  [hawk "0.2.11"]
 
                  ;; avoid conflict with jsonista
-                 [scicloj/gorilla-notes "0.5.3"
+                 [scicloj/gorilla-notes "0.5.5"
                   :exclusions [com.fasterxml.jackson.core/jackson-core
                                com.fasterxml.jackson.dataformat/jackson-dataformat-cbor
                                com.fasterxml.jackson.dataformat/jackson-dataformat-smile]]
