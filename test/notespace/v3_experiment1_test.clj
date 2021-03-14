74d8b6a703934057e602b7325491e42a0b986ed9
daslu <daniel.slutsky@gmail.com>
2021-03-14 15:27:28 +0200

    tutorial cleanup

--- test/notespace/v3_experiment1_test.clj
+++ test/notespace/v3_experiment1_test.clj
@@ -373,14 +373,13 @@ Here are some examples."]
 ["The `vega` kind supports both Vega and Vega-Lite visualizations."]
 
 ^kind/vega
-(do (Thread/sleep 3000)
-    {:description "A simple bar chart with embedded data."
-     :data        {:values [{:a "A" :b 28} {:a "B" :b 55} {:a "C" :b 43}
-                            {:a "D" :b 91} {:a "E" :b 81} {:a "F" :b 53}
-                            {:a "G" :b 19} {:a "H" :b 87} {:a "I" :b 52}]}
-     :mark        :bar
-     :encoding    {:x {:field :a :type :nominal :axis {:labelAngle 0}}
-                   :y {:field :b :type :quantitative}}})
+{:description "A simple bar chart with embedded data."
+ :data        {:values [{:a "A" :b 28} {:a "B" :b 55} {:a "C" :b 43}
+                        {:a "D" :b 91} {:a "E" :b 81} {:a "F" :b 53}
+                        {:a "G" :b 19} {:a "H" :b 87} {:a "I" :b 52}]}
+ :mark        :bar
+ :encoding    {:x {:field :a :type :nominal :axis {:labelAngle 0}}
+               :y {:field :b :type :quantitative}}}
 
 ["### code"]
 
