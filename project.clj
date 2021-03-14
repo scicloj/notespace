22b2660aca32294ea8b6e54867d1ec9d68dd2796
daslu <daniel.slutsky@gmail.com>
2021-03-14 15:23:24 +0200

    updated gorilla-notes dep

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [hawk "0.2.11"]
 
                  ;; avoid conflict with jsonista
-                 [scicloj/gorilla-notes "0.5.6"
+                 [scicloj/gorilla-notes "0.5.9"
                   :exclusions [com.fasterxml.jackson.core/jackson-core
                                com.fasterxml.jackson.dataformat/jackson-dataformat-cbor
                                com.fasterxml.jackson.dataformat/jackson-dataformat-smile]]
