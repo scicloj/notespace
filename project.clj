842d840b5f6a750d0bd7a3ab739645297fb8cf29
daslu <daniel.slutsky@gmail.com>
2021-05-25 16:27:52 +0300

    removed unnecessary hawk dep

--- project.clj
+++ project.clj
@@ -10,7 +10,6 @@
                  [org.clojure/core.cache "1.0.207"]
                  [babashka/fs "0.0.1"]
                  [org.clojure/tools.namespace "1.1.0"]
-                 [hawk "0.2.11"]
 
                  ;; avoid conflict with jsonista
                  [scicloj/gorilla-notes "0.5.12"
