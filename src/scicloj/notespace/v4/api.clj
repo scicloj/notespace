d5d2ae67a7dffbf68881e2013d4d4c4bbb1d1b9b
daslu <daniel.slutsky@gmail.com>
2021-09-26 20:45:59 +0300

    cleanup

--- src/scicloj/notespace/v4/api.clj
+++ src/scicloj/notespace/v4/api.clj
@@ -1,8 +1,7 @@
 (ns scicloj.notespace.v4.api
   (:require [scicloj.notespace.v4.state :as v4.state]
             [scicloj.notespace.v4.watch :as v4.watch]
-            [scicloj.notespace.v4.system :as v4.system]
-            [gorilla-notes.core :as gn]))
+            [scicloj.notespace.v4.system :as v4.system]))
 
 (defn start [{:keys [port open-browser?]}]
   (v4.state/start!)
