c950cc5efabd6dbf42069ab50935bb10c3bd67f4
daslu <daniel.slutsky@gmail.com>
2021-09-28 16:02:08 +0300

    initializing state on restart

--- src/scicloj/notespace/v4/api.clj
+++ src/scicloj/notespace/v4/api.clj
@@ -1,7 +1,9 @@
 (ns scicloj.notespace.v4.api
-  (:require [scicloj.notespace.v4.system :as v4.system]))
+  (:require [scicloj.notespace.v4.state :as v4.state]
+            [scicloj.notespace.v4.system :as v4.system]))
 
 (defn restart! [config]
+  (v4.state/init!)
   (v4.system/restart! config))
 
 
--- src/scicloj/notespace/v4/state.clj
+++ src/scicloj/notespace/v4/state.clj
@@ -1,20 +1,34 @@
 (ns scicloj.notespace.v4.state
   (:require [scicloj.notespace.v4.log :as v4.log]
             [scicloj.notespace.v4.view :as v4.view]
+            [scicloj.notespace.v4.note :as v4.note]
             [scicloj.notespace.v4.frontend.change :as v4.frontend.change]
             [scicloj.notespace.v4.messages :as v4.messages]
+            [scicloj.notespace.v4.path :as v4.path]
             [editscript.core :as editscript])
   (:import java.util.Date))
 
-(defonce *state (atom {:event-counter    0
-                       :last-value       nil
-                       :path->notes      {}
-                       :request-id->path {}
-                       :current-path     nil
-                       :pipeline         nil}))
+(def initial-state {:event-counter    0
+                    :last-value       nil
+                    :path->notes      {}
+                    :request-id->path {}
+                    :current-path     nil
+                    :pipeline         nil})
+
+(defonce *state (atom initial-state))
+
+
+(defn init! []
+  (reset! *state initial-state)
+  ;; (swap! *state
+  ;;        assoc
+  ;;        :last-value nil
+  ;;        :path->notes {}
+  ;;        :request-id->path {}
+  ;;        :current-path nil)
+  )
 
 (defonce *messages (atom []))
-
 
 (defn next-event-counter []
   (swap! *state update :event-counter inc)
