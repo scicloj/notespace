9f4b97ad4be166c5c3de60c77aeeeceb24591c9f
daslu <daniel.slutsky@gmail.com>
2021-10-09 18:11:25 +0300

    typo

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
                   [com.fasterxml.jackson.core/jackson-core "2.10.0"]
                   [com.fasterxml.jackson.core/jackson-annotations "2.10.0"]
                   [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.10.0"]
-                  [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.10.0"]]
+                  [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.10.0"]]]
   :profiles {:dev {:cloverage    {:runner :midje}
                    :dependencies [[midje "1.9.9"]
                                   [aerial.hanami "0.12.7"]]
