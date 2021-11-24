0838bdcb2badd4377f4f3d27a6653be567dac8a2
daslu <daniel.slutsky@gmail.com>
2021-11-24 16:59:15 +0200

    4-alpha-16: tab fixes and minor changes,  aesthetics changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scicloj/notespace "4-alpha-16-SNAPSHOT"
+(defproject scicloj/notespace "4-alpha-16"
   :description "Notebook experience in your Clojure namespace."
   :url "http://github.com/scicloj/notespace"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
